005de882dd8c7a79f2f49e050e26b3249d13522e
atul-github <atul_pandit@yahoo.com>
2018-05-02 11:45:16 +0530

    fixing eslint in test

--- test/server/server.js
+++ test/server/server.js
@@ -1,7 +1,5 @@
 var oecloud = require('oe-cloud');
-var loopback = require('loopback');
-
 oecloud.boot(__dirname, function (err) {
-    oecloud.start();
-    oecloud.emit('test-start');
+  oecloud.start();
+  oecloud.emit('test-start');
 });
--- test/server/test.js
+++ test/server/test.js
@@ -2,8 +2,8 @@ var oecloud = require('oe-cloud');
 var loopback = require('loopback');
 
 oecloud.boot(__dirname, function (err) {
-    oecloud.start();
-    oecloud.emit('test-start');
+  oecloud.start();
+  oecloud.emit('test-start');
 });
 
 var chalk = require('chalk');
@@ -20,83 +20,83 @@ var supertest = require('supertest');
 
 var api = defaults(supertest(app));
 var basePath = app.get('restApiRoot');
-productCatalogUrl=basePath + '/ProductCatalogs';
+var productCatalogUrl = basePath + '/ProductCatalogs';
 describe(chalk.blue('service personalization test started...'), function () {
   this.timeout(80000);
   before('wait for boot scripts to complete', function (done) {
-      app.on('test-start', function () {
-          ProductCatalog = loopback.findModel('ProductCatalog');
-          ProductCatalog.destroyAll(function (err, info) {
-              return done(err);
-          });
+    app.on('test-start', function () {
+      ProductCatalog = loopback.findModel('ProductCatalog');
+      ProductCatalog.destroyAll(function (err, info) {
+        return done(err);
       });
+    });
   });
   PersonalizationRule = loopback.findModel('PersonalizationRule');
   it('service personalization test - create test data', function (done) {
-      // Populate some data.
-      var item1 = {
-          'name': 'king size bed',
-          'category': 'furniture',
-          'desc': 'king size bed',
-          'price': {
-              'value': 10000,
-              'currency': 'inr'
-          },
-          'isAvailable': true
-      };
-      var item2 = {
-          'name': 'office chair',
-          'category': 'furniture',
-          'desc': 'office chair',
-          'price': {
-              'value': 5000,
-              'currency': 'inr'
-          },
-          'isAvailable': true
-      };
-      var item3 = {
-          'name': 'dinning table',
-          'category': 'furniture',
-          'desc': 'dinning table',
-          'price': {
-              'value': 8000,
-              'currency': 'inr'
-          },
-          'isAvailable': false
-      };
-      var item11 = {
-          'name': 'refrigerator',
-          'category': 'electronics',
-          'desc': 'refrigerator',
-          'price': {
-              'value': 10000,
-              'currency': 'inr'
-          },
-          'isAvailable': true
-      };
-      var item22 = {
-          'name': 'water heater',
-          'category': 'electronics',
-          'desc': 'water heater',
-          'price': {
-              'value': 5000,
-              'currency': 'inr'
-          },
-          'isAvailable': true
-      };
-      var item33 = {
-          'name': 'oven',
-          'category': 'electronics',
-          'desc': 'oven',
-          'price': {
-              'value': 8000,
-              'currency': 'inr'
-          },
-          'isAvailable': false
-      };
-      ProductCatalog.create([item1, item2, item3, item11, item22, item33], function (err, results) {
-          return done(err);
-      });
+    // Populate some data.
+    var item1 = {
+      'name': 'king size bed',
+      'category': 'furniture',
+      'desc': 'king size bed',
+      'price': {
+        'value': 10000,
+        'currency': 'inr'
+      },
+      'isAvailable': true
+    };
+    var item2 = {
+      'name': 'office chair',
+      'category': 'furniture',
+      'desc': 'office chair',
+      'price': {
+        'value': 5000,
+        'currency': 'inr'
+      },
+      'isAvailable': true
+    };
+    var item3 = {
+      'name': 'dinning table',
+      'category': 'furniture',
+      'desc': 'dinning table',
+      'price': {
+        'value': 8000,
+        'currency': 'inr'
+      },
+      'isAvailable': false
+    };
+    var item11 = {
+      'name': 'refrigerator',
+      'category': 'electronics',
+      'desc': 'refrigerator',
+      'price': {
+        'value': 10000,
+        'currency': 'inr'
+      },
+      'isAvailable': true
+    };
+    var item22 = {
+      'name': 'water heater',
+      'category': 'electronics',
+      'desc': 'water heater',
+      'price': {
+        'value': 5000,
+        'currency': 'inr'
+      },
+      'isAvailable': true
+    };
+    var item33 = {
+      'name': 'oven',
+      'category': 'electronics',
+      'desc': 'oven',
+      'price': {
+        'value': 8000,
+        'currency': 'inr'
+      },
+      'isAvailable': false
+    };
+    ProductCatalog.create([item1, item2, item3, item11, item22, item33], function (err, results) {
+      return done(err);
+    });
   });
 
   afterEach('destroy context', function (done) {
@@ -107,63 +107,63 @@ describe(chalk.blue('service personalization test started...'), function () {
       }
     };
     PersonalizationRule.destroyAll({}, callContext, function (err, result) {
-      //console.log("Personalization Rule Model Removed : ", err, result);
+      // console.log("Personalization Rule Model Removed : ", err, result);
       done();
     });
   });
 
   it('t1 should replace field names in response when fieldReplace personalization is configured', function (done) {
-      // Setup personalization rule
-      var ruleForAndroid = {
-          'modelName': 'ProductCatalog',
-          'personalizationRule': {
-              'fieldReplace': {
-                  'name': 'product name',
-                  'desc': 'product description',
-              }
-          },
-          'scope': {
-              'device': 'android'
-          }
-      };
-
-  PersonalizationRule.create(ruleForAndroid, function (err, rule) {
-    if (err) {
-        return done(new Error(err));
-    }
-    //var ruleId = rule.id;
-    api.get(productCatalogUrl)
-      .set('Accept', 'application/json')
-      .set('REMOTE_USER', 'testUser')
-      .set('device', 'android')
-      .expect(200).end(function (err, resp) {
-        if (err) {
-            done(err);
+    // Setup personalization rule
+    var ruleForAndroid = {
+      'modelName': 'ProductCatalog',
+      'personalizationRule': {
+        'fieldReplace': {
+          'name': 'product name',
+          'desc': 'product description'
         }
-        var results = resp.body; //JSON.parse(resp.text);
+      },
+      'scope': {
+        'device': 'android'
+      }
+    };
 
-        expect(results.length).to.be.equal(6);
-        expect(results[0])
-          .to.include.keys('product name', 'product description');
-        expect(results[0])
-          .to.not.include.keys('name', 'desc');
-        done();
-      });
+    PersonalizationRule.create(ruleForAndroid, function (err, rule) {
+      if (err) {
+        return done(new Error(err));
+      }
+      // var ruleId = rule.id;
+      api.get(productCatalogUrl)
+        .set('Accept', 'application/json')
+        .set('REMOTE_USER', 'testUser')
+        .set('device', 'android')
+        .expect(200).end(function (err, resp) {
+          if (err) {
+            done(err);
+          }
+          var results = resp.body;
+
+          expect(results.length).to.be.equal(6);
+          expect(results[0])
+            .to.include.keys('product name', 'product description');
+          expect(results[0])
+            .to.not.include.keys('name', 'desc');
+          done();
+        });
     });
   });
 
   var owner1 = {
-    "name": "John",
-    "city": "Miami"
+    'name': 'John',
+    'city': 'Miami'
   };
 
   var owner2 = {
-    "name": "Wick",
-    "city": "Texas"
+    'name': 'Wick',
+    'city': 'Texas'
   };
 
   it('t2 create records in product owners', function (done) {
-    ProductOwner=loopback.findModel('ProductOwner');
+    ProductOwner = loopback.findModel('ProductOwner');
     ProductOwner.create(owner1, function (err) {
       if (err) {
         return done(err);
@@ -171,8 +171,8 @@ describe(chalk.blue('service personalization test started...'), function () {
       ProductOwner.create(owner2, function (err) {
         return done(err);
       });
-    });    
-  });    
+    });
+  });
 
   it('t3 should replace field names in response when fieldReplace personalization is configured', function (done) {
     // Setup personalization rule
@@ -195,56 +195,56 @@ describe(chalk.blue('service personalization test started...'), function () {
         return done(err);
       }
       api.get(productCatalogUrl)
-      .set('Accept', 'application/json')
-      .set('REMOTE_USER', 'testUser')
-      .set('device', 'android')
-      .expect(200).end(function (err, resp) {
-        if (err) {
-            done(err);
-        }
-        var results = resp.body; //JSON.parse(resp.text);
-        expect(results.length).to.be.equal(6);
-        expect(results[0]).to.include.keys('isAvailable');
-        expect(results[0].isAvailable).to.be.oneOf([0, 1]);
-        done();
-      });
-    });
-  });
-
-  //sort test cases
-  it('t4 single sort condition:  should return the sorted result when sort personalization rule is configured.', function (done) {
-      // Setup personalization rule
-      var ruleForAndroid = {
-        'modelName': 'ProductCatalog',
-        'personalizationRule': {
-          'sort': {
-            'name': 'asc'
-          }
-        },
-        'scope': {
-          'device': 'android'
-        }
-      };
-      PersonalizationRule.create(ruleForAndroid, {}, function (err, rule) {
-        if (err) {
-          return done(err);
-        }
-        api.get(productCatalogUrl)
         .set('Accept', 'application/json')
         .set('REMOTE_USER', 'testUser')
         .set('device', 'android')
-          .expect(200).end(function (err, resp) {
-            if (err) {
-              done(err);
-            }
-            var results = resp.body;
-            expect(results).to.be.instanceof(Array);
-            expect(results.length).to.equal(6);
-            expect(results[0].name).to.be.equal('dinning table');
-            done();
-          });
-      });
+        .expect(200).end(function (err, resp) {
+          if (err) {
+            done(err);
+          }
+          var results = resp.body;
+          expect(results.length).to.be.equal(6);
+          expect(results[0]).to.include.keys('isAvailable');
+          expect(results[0].isAvailable).to.be.oneOf([0, 1]);
+          done();
+        });
     });
+  });
+
+  // sort test cases
+  it('t4 single sort condition:  should return the sorted result when sort personalization rule is configured.', function (done) {
+    // Setup personalization rule
+    var ruleForAndroid = {
+      'modelName': 'ProductCatalog',
+      'personalizationRule': {
+        'sort': {
+          'name': 'asc'
+        }
+      },
+      'scope': {
+        'device': 'android'
+      }
+    };
+    PersonalizationRule.create(ruleForAndroid, {}, function (err, rule) {
+      if (err) {
+        return done(err);
+      }
+      api.get(productCatalogUrl)
+        .set('Accept', 'application/json')
+        .set('REMOTE_USER', 'testUser')
+        .set('device', 'android')
+        .expect(200).end(function (err, resp) {
+          if (err) {
+            done(err);
+          }
+          var results = resp.body;
+          expect(results).to.be.instanceof(Array);
+          expect(results.length).to.equal(6);
+          expect(results[0].name).to.be.equal('dinning table');
+          done();
+        });
+    });
+  });
 
   it('t5 single sort condition: should sort in ascending order when the sort order is not specified', function (done) {
     // Setup personalization rule
@@ -275,13 +275,13 @@ describe(chalk.blue('service personalization test started...'), function () {
           if (err) {
             return done(err);
           }
-          var results = resp.body
+          var results = resp.body;
           expect(results).to.be.instanceof(Array);
           expect(results.length).to.equal(6);
           expect(results[0].name).to.be.equal('dinning table');
           done();
-      });
+        });
     });
-  });    
+  });
 });
 
